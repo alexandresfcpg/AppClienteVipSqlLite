f7b48ccc9b6c6e3c9ee9981fff5d04dadade883f
alexandresfcpg <alexandresfcpg@yahoo.com.br>
2024-08-30 15:48:48 -0300

    Implementando a inserção do email e da senha no BD, mas deverá ser corrigido.

--- app/src/main/java/app/daazi/aluno/appclientevipsqllite/view/ClienteVipActivity.java
+++ app/src/main/java/app/daazi/aluno/appclientevipsqllite/view/ClienteVipActivity.java
@@ -138,7 +138,7 @@ public class ClienteVipActivity extends AppCompatActivity {
         dados.putString("primeiroNome", novoVip.getPrimeiroNome());
         dados.putString("sobreNome", novoVip.getSobreNome());
         dados.putBoolean("pessoaFisica", novoVip.isPessoaFisica());
-        dados.putInt("ultimoID", ultimoID);
+        dados.putInt("clienteID", ultimoID);
         dados.apply();
 
     }
